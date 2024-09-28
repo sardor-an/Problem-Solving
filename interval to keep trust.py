83ea40c7b96d26719705e3d786512e3ac56c901c
sardor-an <sardorxakimov200@gmail.com>
2024-09-28 21:41:45 +0500

    I have found why 12; 1 is "wrong"

--- interval to keep trust.py
+++ interval to keep trust.py
@@ -1,1 +1,9 @@
 # Nothing can stop me and I never stop.
+
+n, m = map(int, input().split())
+day = n
+
+while n != 0:
+    day += 1
+
+    
--- save the trust.py
+++ save the trust.py
@@ -1,14 +1,14 @@
 # Kachok
-# n, m = map(int, input().split())
-# eggs = n
-# day = 0
-# while n != 0:
-#     day += 1
-#     if day == m or day % m == 0:
-#         eggs += 1
-#         n += 1
-#     n -= 1
-# print(day)
+n, m = map(int, input().split())
+eggs = n
+day = 0
+while n != 0:
+    day += 1
+    if day == m or day % m == 0:
+        eggs += 1
+        n += 1
+    n -= 1
+print(day)
 
 # Zapravka
 # n, a, b = map(int, input().split())
@@ -16,13 +16,13 @@
 
 
 
-s = 100
-m = 1
-n = 0
-r = 0
-while s > r:
-    r = m + n
-    print(r)
-    n = m
-    m = r
-    s -= 1
+# s = 100
+# m = 1
+# n = 0
+# r = 0
+# while s > r:
+#     r = m + n
+#     print(r)
+#     n = m
+#     m = r
+#     s -= 1
