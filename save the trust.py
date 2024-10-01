ffea2402da9ce0f2592dbefc28f68e48da6eb17e
sardor-an <sardorxakimov200@gmail.com>
2024-10-01 19:02:00 +0500

    hard things are not for tomorrow, they are going to be done by my now.

--- save the trust.py
+++ save the trust.py
@@ -30,17 +30,38 @@
 # --------------------------------------------------------------------------
 # Mirror 5-6-7
     # B
-def check(y):
-    y = list(map(int, list(str(y))))
-    n = 0
-    while n <= len(y)-1:
-        if y.count(y[n]) > 1:
-            return False
-        else:
-            n += 1
-    return True
-y = int(input())+1
-while not check(y):
-    y += 1
-print(y)
+# def check(y):
+#     y = list(map(int, list(str(y))))
+#     n = 0
+#     while n <= len(y)-1:
+#         if y.count(y[n]) > 1:
+#             return False
+#         else:
+#             n += 1
+#     return True
+# y = int(input())+1
+# while not check(y):
+#     y += 1
+# print(y)
 
+# ------------------------------------------------------------------------- DAY 4
+    # C
+# l = int(input())
+# k = list(map(int, input().split()))
+# l = 9
+# k = [3,1,2, 1,2,3, 2,3,1]
+
+# temp = k[:3]*(l//3)
+# t = 0
+# for n in range(0, l):
+#     if temp[n] != k[n]:
+#         t += 1
+# print(t)
+
+# n, m = map(int, input().split())
+# b = 0
+# a = (m - b) ** 0.5
+# while not a + (b**2) == n:
+#    b += 1
+# print(b)
+  
