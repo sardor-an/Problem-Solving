518a9c9a2f9a395ce8244c43839e3e6fd9c46193
sardor-an <sardorxakimov200@gmail.com>
2024-10-27 21:33:10 +0500

    just try and solve.

---
+++ main.py
@@ -0,0 +1,19 @@
+# Viloyat 2023 11-sinf
+
+# def manhattan(n):
+#     """ |a-c| + |b-d| """
+
+#     xy_pairs = []
+#     for k in range(n):
+#         x,y = map(int, input().split())
+#         xy_pairs.append([x,y])
+#     r_min, r_max = 0,0
+#     for i in xy_pairs:
+#         for j in xy_pairs:
+#             if abs(i[0] - j[0]) + abs(i[1] - j[1]) > ...:
+#                 ...
+    
+
+    
+# print(manhattan(4))
+print(...)
