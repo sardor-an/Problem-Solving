3233743e1ced93826b807c2bb1b9f5a78792ae8d
sardor-an <sardorxakimov200@gmail.com>
2024-11-22 22:26:14 +0500

    Today I am going to solve 10 non-empty problems and prove fucking

--- 1-2-3-4-5.py
+++ 1-2-3-4-5.py
@@ -55,19 +55,40 @@
 #     elif n <= 9 and n >= 5: i = 5
 #     else: i = 1
 #     return i
-n = int(input())
-a = list(map(int, input().split()))
-def fuck_hanif_pro(n, a):
-    a = list(reversed(sorted(a)))
-    for k in range(len(a)):
-        if n >= a[k] :
-            return a[k]
+# n = int(input())
+# a = list(map(int, input().split()))
+# def fuck_hanif_pro(n, a):
+#     a = list(reversed(sorted(a)))
+#     for k in range(len(a)):
+#         if n >= a[k] :
+#             return a[k]
+# i = 0
+# while n != 0:
+#     m = fuck_hanif_pro(n, a)
+#     i += n // m
+#     n -= (n // m) * m
+#     if n in a:
+#         i += 1
+#         break
+# print(i)
+
+
+x1, y1, x2, y2 = map(int, input().split())
+m = 1 if y1 < y2 else -1
 i = 0
-while n != 0:
-    m = fuck_hanif_pro(n, a)
-    i += n // m
-    n -= (n // m) * m
-    if n in a:
+flag = True
+while flag:
+    if x1 == x2:
+        i += abs(y1 - y2)
+        print('one', y1 - y2)
+        flag = False
+    elif y1 == y2:
+        i += abs(x1 - x2)
+        print('two')
+        flag = False
+    else:
+        x1 += m
+        y1 += m
         i += 1
-        break
-print(i)
+        print('three')
+print(int(i * 0.5))
